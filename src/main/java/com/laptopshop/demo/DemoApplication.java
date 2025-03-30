2c59b355249cad970250fb33f6220e30628341e3
Toan0409 <Tranquoctoan04092004@gmail.com>
2025-03-30 22:38:39 +0700

    30/3

--- src/main/java/com/laptopshop/demo/DemoApplication.java
+++ src/main/java/com/laptopshop/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.laptopshop.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/main/java/com/laptopshop/demo/HomeController.java
+++ src/main/java/com/laptopshop/demo/HomeController.java
@@ -1,13 +1,24 @@
 package com.laptopshop.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello World from spring boot";
+    @GetMapping("/")
+    public String index() {
+        return "Hello World  ";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 }
